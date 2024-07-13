2f2a4656f858bd76c17f03999582dc57dfcf4250
olympusmons1256 <142126541+olympusmons1256@users.noreply.github.com>
2024-07-13 15:06:13 +0000

    R21

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import SideMenu from './components/SideMenu';
-import InputModule from './components/InputModule';
-import CanvasArea from './components/CanvasArea';
-import ProtectedRoute from './components/ProtectedRoute';
-import SearchPage from './components/SearchPage';
-import SSOLogin from './components/SSOLogin';
-import UserProfile from './components/UserProfile';
-
-function App() {
-  return (
-    <Router>
-      <div className="app-container">
-        <SideMenu />
-        <div className="main-area">
-          <CanvasArea />
-          <InputModule />
-        </div>
-      </div>
-      <Routes>
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/login" element={<SSOLogin />} />
-        <Route path="/profile" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
-        <Route path="*" element={<Navigate to="/search" replace />} />
-      </Routes>
-    </Router>
-  );
+/* Add component-specific styles here */
+.side-menu {
+  width: 200px;
+  background-color: #343a40;
+  color: #fff;
+  padding: 1rem;
 }
 
-export default App;
+.input-module {
+  padding: 1rem;
+  background-color: #f8f9fa;
+}
