f02178a4f00e957adaabaed78d70e6b8209def2b
olympusmons1256 <142126541+olympusmons1256@users.noreply.github.com>
2024-07-13 14:32:27 +0000

    R3

---
+++ src/components/InputModule.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Textarea } from 'react-spectrum/Textarea';
+
+const InputModule = () => {
+  return (
+    <div className="input-module">
+      <Textarea placeholder="Enter your input here" />
+    </div>
+  );
+};
+
+export default InputModule;
