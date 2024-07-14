e8ef8a4c967ee85710c3dffaaabaac94a60085ae
olympusmons1256 <142126541+olympusmons1256@users.noreply.github.com>
2024-07-14 05:39:41 +0000

    "Reorganize component files into a dedicated components folder"

--- src/components/dashboard/MainContainer.jsx
+++ src/components/dashboard/MainContainer.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import LeftMenu from "../LeftMenu";
+import "../../styles/MainContainer.css"; // Import the CSS file from the styles folder
 
 export default function MainContainer() {
   const navigate = useNavigate();
