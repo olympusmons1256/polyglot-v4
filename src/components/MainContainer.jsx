1aa59c4a0fa28186e8a83f0eaf5f32d337433aae
olympusmons1256 <142126541+olympusmons1256@users.noreply.github.com>
2024-07-14 05:47:28 +0000

    MainContainer.jsx

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import LeftMenu from "./LeftMenu";
-import "../styles/MainContainer.css"; // Import the CSS file from the styles folder
-
 export default function MainContainer() {
   const navigate = useNavigate();
 
@@ -9,6 +5,3 @@ export default function MainContainer() {
     <div>
       <LeftMenu />
       {/* Add your main content here */}
-    </div>
-  );
-}
